b80199cee2436b7db28c254d04f05194619f5350
Luis Fernando Florez <ing_luis_florez@hotmail.com>
2024-04-04 19:13:53 -0500

    add basic countries api controller

---
+++ Gestional/Gestional.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,58 @@
+﻿using Gestional.Backend.Data;
+using Gestional.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestional.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("id")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if(country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);          
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
